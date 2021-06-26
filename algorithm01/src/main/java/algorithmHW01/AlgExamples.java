baa062af915beaa483d0d2661a3e1b11e41fc768
Tigg <ignatovichanastasiia@gmail.com>
2021-06-26 16:59:47 +0300

    algorithm

---
+++ algorithm01/src/main/java/algorithmHW01/AlgExamples.java
@@ -0,0 +1,16 @@
+package algorithmHW01;
+
+public class AlgExamples {
+    public static void main(String[] args) {
+
+    }
+    public class Person{
+        private String name;
+        private Integer i;
+
+        public Person(){
+            this.name = String.valueOf(i+1);
+        }
+    }
+}
+
